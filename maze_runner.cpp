e7ce08fe6d8e41ea7110166c44125f66ad5aa068
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-22 21:50:09 -0400

    Create maze_runner.cpp

---
+++ maze_runner.cpp
@@ -0,0 +1,59 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: Project 1 Task B
+I wrote a program that initializes the map and stream in a list of directions as to determine whether or not it correctly traverses the maze..
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+
+{
+int maze[6][6] = 
+{
+1,0,1,1,1,1,
+1,0,0,0,1,1,
+1,0,1,0,0,1,
+1,0,1,0,1,1,
+1,0,0,0,0,0,
+1,1,1,1,1,1
+};
+
+int i=-1,j=0,x=0;             
+
+char f[100];                   
+
+cin.getline(f,100);            
+
+while(f[x] != '\0')           
+{
+
+if (f[x] =='D')  
+{
+  i++;
+}
+if (f[x] == 'U')                 
+{
+          i--;
+}
+if(f[x] == 'R')                 
+{
+	j++;
+}
+if(f[x] == 'L')                 
+{
+	j--;
+}
+x++;
+}
+
+if (i == 3 && j == 4)         
+cout<<"You got out of the maze.";
+else
+cout<<"You are stuck in the maze.";
+
+return 0;
+
+}
